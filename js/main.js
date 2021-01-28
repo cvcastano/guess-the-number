d0b56f1cd4f513742f399c69f298866d2c248a65
cvcastano <carocastanocastillo@gmail.com>
2021-01-28 11:08:59 +0100

    add final smart version

--- js/main.js
+++ js/main.js
@@ -5,16 +5,20 @@ const button = document.querySelector('.js-button');
 const hintElement = document.querySelector('.js-hint');
 const counterElement = document.querySelector('.js-counter');
 
-
+function superFunGame () {
+    incrCounter ();
+    narrowNumberDown ();
+}
 
 const incrCounter = () => {
     let value = parseInt(counterElement.innerHTML);
     value++;
     counterElement.innerHTML = value;
+}
 
+function narrowNumberDown (){
     const inputElement = parseInt(document.querySelector('.js-input').value);
     console.log(inputElement);
-
 
     if (inputElement > 100 || inputElement < 1) {
         hintElement.innerHTML = 'It must be a number between 1 and 100'
@@ -30,4 +34,4 @@ const incrCounter = () => {
 const randomNumber = Math.ceil(Math.random() * 100);
 console.log(`The random number to guess is ${randomNumber}`);
 
-button.addEventListener('click', incrCounter);
+button.addEventListener('click', superFunGame);
