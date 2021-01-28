b8f9915ee869114296c800bf0770a9969d360f34
cvcastano <carocastanocastillo@gmail.com>
2021-01-28 09:15:24 +0100

    add counter

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const inputElement = document.querySelector('.js-input');
+const button = document.querySelector('.js-button');
+const hintElement = document.querySelector('.js-hint');
+const counterElement = document.querySelector('.js-counter');
+
+
+//contador:
+const incrCounter = () => {
+    let value = parseInt(counterElement.innerHTML);
+    value++;
+    counterElement.innerHTML = value;
+    console.log ('me han clickado');
+}
+button.addEventListener ('click', incrCounter);
