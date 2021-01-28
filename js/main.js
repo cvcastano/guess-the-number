f93e4ffcbbc73e217327c382b3e01d36172d9b04
cvcastano <carocastanocastillo@gmail.com>
2021-01-28 10:22:04 +0100

    add conditional to narrow number down

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const inputElement = document.querySelector('.js-input');
+
 const button = document.querySelector('.js-button');
 const hintElement = document.querySelector('.js-hint');
 const counterElement = document.querySelector('.js-counter');
@@ -11,13 +11,30 @@ const incrCounter = () => {
     let value = parseInt(counterElement.innerHTML);
     value++;
     counterElement.innerHTML = value;
-    console.log ('me han clickado');
-}
+    console.log('me han clickado');
+
+    //número que se escribe en el input:
+    const inputElement = parseInt(document.querySelector('.js-input').value);
+    console.log(inputElement);
+
+    //condicional para acotar el número:
+    if (inputElement > 100 || inputElement < 1) {
+           hintElement.innerHTML = 'El número debe estar entre 1 y 100'
+        } else if (inputElement === randomNumber) {
+            hintElement.innerHTML = 'Has ganado, campeona!!!'
+        } else if (inputElement > randomNumber) {
+            hintElement.innerHTML = 'Demasiado alto'
+        } else if (inputElement < randomNumber) {
+            hintElement.innerHTML = 'Demasiado bajo'
+        }
+    }
 
 
-   
-//número random como referencia: (ejecutarse una sóla vez --> fuera)
-const randomNumber = Math.ceil(Math.random()*100);
+
+
+
+//número random como referencia: (ejecutarse una sóla vez --> fuera) PERO AL CLICKAR, NO ANTES, NO?
+const randomNumber = Math.ceil(Math.random() * 100);
 console.log(randomNumber);
-   
-button.addEventListener ('click', incrCounter);
+
+button.addEventListener('click', incrCounter);
