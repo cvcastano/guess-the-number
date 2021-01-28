37f914ccb4986c69aee674b5f9b5837edc2430c7
cvcastano <carocastanocastillo@gmail.com>
2021-01-28 10:38:46 +0100

    change page to English

--- js/main.js
+++ js/main.js
@@ -5,36 +5,33 @@ const button = document.querySelector('.js-button');
 const hintElement = document.querySelector('.js-hint');
 const counterElement = document.querySelector('.js-counter');
 
+function superFunGame () {
+    incrCounter();
+    inputElement();
+}
 
-//contador:
 const incrCounter = () => {
     let value = parseInt(counterElement.innerHTML);
     value++;
     counterElement.innerHTML = value;
-    console.log('me han clickado');
 
-    //número que se escribe en el input:
+//número que se escribe en el input:
     const inputElement = parseInt(document.querySelector('.js-input').value);
     console.log(inputElement);
 
-    //condicional para acotar el número:
+//condicional para acotar el número:
     if (inputElement > 100 || inputElement < 1) {
-           hintElement.innerHTML = 'El número debe estar entre 1 y 100'
-        } else if (inputElement === randomNumber) {
-            hintElement.innerHTML = 'Has ganado, campeona!!!'
-        } else if (inputElement > randomNumber) {
-            hintElement.innerHTML = 'Demasiado alto'
-        } else if (inputElement < randomNumber) {
-            hintElement.innerHTML = 'Demasiado bajo'
-        }
+        hintElement.innerHTML = 'It must be a number between 1 and 100'
+    } else if (inputElement === randomNumber) {
+        hintElement.innerHTML = 'You\'re the winner!!!'
+    } else if (inputElement > randomNumber) {
+        hintElement.innerHTML = 'Too high, try a lower number'
+    } else if (inputElement < randomNumber) {
+        hintElement.innerHTML = 'Too low, try a higher number'
     }
+}
 
-
-
-
-
-//número random como referencia: (ejecutarse una sóla vez --> fuera) PERO AL CLICKAR, NO ANTES, NO?
 const randomNumber = Math.ceil(Math.random() * 100);
-console.log(randomNumber);
+console.log(`The random number to guess is ${randomNumber}`);
 
 button.addEventListener('click', incrCounter);
